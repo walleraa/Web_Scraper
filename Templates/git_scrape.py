b7604bfa7f3918d74f702dee9ba06fb318633ed7
walleraa <walleraa@umich.edu>
2023-11-03 13:29:54 -0400

    Finished git_scrape

--- Templates/git_scrape.py
+++ Templates/git_scrape.py
@@ -9,5 +9,5 @@ def scraper(attribute):
     return {
         "File Name": attribute.find("div", attrs={"class":"col-md-2"}).find("span").find("a").text.strip(),
         "Last Commit": attribute.find("div", attrs={"class":"col-5"}).find("span").find("a").text.strip(),
-        "Commit Time": attribute.find("div", attrs={"class":"text-right"})
+        "Commit Time": attribute.find("div", attrs={"class":"text-right"}).text.strip()
     }
